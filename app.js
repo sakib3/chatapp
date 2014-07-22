1693f548048b5e1f2eb0527fd7df9ade5b75b6cd
Sabbir Rahman Sakib <sakib@Sabbirs-MacBook-Pro.local>
2014-07-22 13:16:11 +0200

    Adding Usernames

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
-	io = require('socket.io').listen(server);
+	io = require('socket.io').listen(server)
+	nicknames = [];
 
 server.listen(3000);
 
@@ -13,10 +14,34 @@ app.get('/', function(req, res){
 
 //receive the event from client
 io.sockets.on('connection', function(socket){
+	socket.on('new user', function(data, callback){
+		if(nicknames.indexOf(data) !=-1){
+			callback(false);
+		} else{
+			callback(true);
+			socket.nickname = data;
+			nicknames.push(socket.nickname);
+			updateNicknames();
+		}
+	});
+
+	function updateNicknames(){
+		io.sockets.emit('usernames', nicknames);
+	}
+	//send message
 	socket.on('send message', function(data){
 		//broadcast the received msg to all including the sender
-		io.sockets.emit('new message', data);
+		io.sockets.emit('new message', { msg:data, nick:socket.nickname });
 		//broadcast the received msg to all except the sender
 		//io.broadcast.emit('new message', data);
-	})
+	});
+
+
+	//Whenever a user disconnect from the server then this events emit
+	socket.on ('disconnect',function(data){
+		if(!socket.nickname) return;
+		//remove that clinet from the array
+		nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+		updateNicknames();
+	});
 });
