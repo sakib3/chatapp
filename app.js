8318109ad28869e2f0caff933ad3040e0287985f
Sabbir Rahman Sakib <sakib@Sabbirs-MacBook-Pro.local>
2014-07-22 15:20:41 +0200

    Private Messaging is done

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+console.log('Server started.');
 var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server)
-	nicknames = [];
+	users = {};
 
 server.listen(3000);
 
@@ -15,33 +16,57 @@ app.get('/', function(req, res){
 //receive the event from client
 io.sockets.on('connection', function(socket){
 	socket.on('new user', function(data, callback){
-		if(nicknames.indexOf(data) !=-1){
+		if(data in users){
 			callback(false);
 		} else{
 			callback(true);
 			socket.nickname = data;
-			nicknames.push(socket.nickname);
+			users[socket.nickname] = socket;
 			updateNicknames();
 		}
 	});
 
 	function updateNicknames(){
-		io.sockets.emit('usernames', nicknames);
+		io.sockets.emit('usernames', Object.keys(users));
 	}
 	//send message
-	socket.on('send message', function(data){
-		//broadcast the received msg to all including the sender
-		io.sockets.emit('new message', { msg:data, nick:socket.nickname });
-		//broadcast the received msg to all except the sender
-		//io.broadcast.emit('new message', data);
+	socket.on('send message', function(data, callback){
+		//remove the whitespace
+		var msg = data.trim();
+		//if first three char is matched then whispering
+		if(msg.substr(0,3) === '/w '){
+			msg = msg.substr(3);
+			//get the index of the space, after whisper name then space and then msg
+			var ind = msg.indexOf(' ');
+			if(ind !== -1){
+				var name = msg.substring(0, ind);
+				var msg = msg.substring(ind + 1 );
+				if(name in users){
+					users[name].emit('whisper', { msg:msg, nick:socket.nickname });
+					//console.log('Whisper!');
+				} else{
+					callback('Error! Enter a valid user.');
+				}
+				
+			} else{
+				callback('Error! Please enter a message for your Whisper.');
+			}
+			
+		} else{
+			//broadcast the received msg to all including the sender
+			io.sockets.emit('new message', { msg:msg, nick:socket.nickname });
+			//broadcast the received msg to all except the sender
+			//io.broadcast.emit('new message', data);
+		}
+		
 	});
 
 
 	//Whenever a user disconnect from the server then this events emit
 	socket.on ('disconnect',function(data){
 		if(!socket.nickname) return;
-		//remove that clinet from the array
-		nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+		//remove that clinet from the object
+		delete users[socket.nickname];
 		updateNicknames();
 	});
 });
